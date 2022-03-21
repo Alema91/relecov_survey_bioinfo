7e0148cb76be506d211d90d9ee1e3d5f84a288d1
Alema91 <alberto.lema.blanco@gmail.com>
2022-03-21 14:00:37 +0100

    Levels for sort data frame

--- _01_analysis.R
+++ _01_analysis.R
@@ -21,20 +21,42 @@ survey_general <- read_excel(dir_excel[1], sheet = 2)
 
 ### datos parsed general ----
 
+level_comunidades <- c("CATALUÑA", "MADRID", "GALICIA", "VALENCIA", "BALEARES", "CANARIAS", "CANTABRIA", "CASTILLA LA MANCHA", "LA RIOJA")
+level_centros <- c(
+    "Hospital Joan XXIII",
+    "Hospital Universitari Vall d’Hebron",
+    "Hospital Dr. Josep Trueta",
+    "Hospital Clínic de Barcelona",
+    "Banc de Sang i Teixits Catalunya",
+    "Hospital Universitario La Paz",
+    "Hospital Universitario Gregorio Marañón",
+    "Hospital Universitario Ramón y Cajal",
+    "Hospital Universitario 12 de Octubre",
+    "Hospital de A Coruña",
+    "Complejo Hospitalario de Santiago de Compostela",
+    "Hospital General Universitario de Alicante",
+    "FISABIO CSIC",
+    "Hospital Universitario Son Espases",
+    "Instituto Tecnológico y de Energías Renovables",
+    "Hospital Universitario Marqués de Valdecilla",
+    "Hospital Universitario de Ciudad Real",
+    "Fundación Rioja Salud"
+)
+
 general_data <- data.frame(
-    id = as.character(survey_general$`Relecov-ID`),
-    comunidad = as.character(survey_general$`Autonomous community`),
-    centro = as.character(survey_general$`Center name`),
+    id = as.character(sort(survey_general$`Relecov-ID`)),
+    comunidad = factor(survey_general$`Autonomous community`, levels = level_comunidades),
+    centro = factor(survey_general$`Center name`, levels = level_centros),
     fecha = excel_numeric_to_date(as.numeric(survey_general$fecha))
 )
 
-#### plot tiempo de ejecucion ----
+#### plot general ----
 
-ggplot(fechas_data, aes(
-    x = recepcion, xend = secuenciacion,
-    y = id, yend = id, color = "#4a8abe"
-)) +
-    geom_segment(size = 3, show.legend = F) +
-    geom_text(aes(label = fechas_data$ejecucion), position = position_dodge(width = 1), hjust = -1, color = "black", size = 2.5) +
-    labs(x = "Execution time (days)", y = "")
-ggsave("Graficos/qc_tiempo_ejecucion.png")
+ggplot(general_data, aes(x = comunidad, fill = centro)) +
+    geom_bar(aes(fill = centro)) +
+    labs(x = "", y = "", title = "") +
+    theme(
+        axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1, size = 6),
+        axis.text.y = element_text(size = 7)
+    )
+ggsave("Graficos/survey_general.png")
